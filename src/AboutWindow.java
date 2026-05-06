c47d6b6d227cdfedb8b2cf6f2a34603dca9f5ac0
0-Cuprum-0 <imaf77198@gmail.com>
2026-05-06 19:08:14 +0200

    Podział na klasy+dodanie nowych plików dla strony about

--- src/AboutWindow.java
+++ src/AboutWindow.java
@@ -1,2 +1,100 @@
-public class AboutWindow {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class AboutWindow extends JFrame {
+    public AboutWindow (){
+        createAboutWindow();
+        initLayout();
+    }
+
+    private void createAboutWindow(){
+
+        setTitle("Wizualizator grafów");
+        setSize(450, 300);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setBackground(AppTheme.BG_COLOR);
+
+
+    }
+
+    public void centerThisText(JLabel label){
+
+        label.setForeground(AppTheme.FG_COLOR);
+        label.setAlignmentX(CENTER_ALIGNMENT);
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+    }
+
+    private void initLayout() {
+        this.getContentPane().setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+        this.getContentPane().setBackground(AppTheme.BG_COLOR); 
+
+        add(Box.createVerticalStrut(20));
+
+        add(createImageIcon());
+
+        add(Box.createVerticalStrut(20));
+
+        JLabel textLabel = new JLabel("Wizualizator Grafów v1.0");
+        centerThisText(textLabel);
+        add(textLabel);
+
+        JLabel authors = new JLabel("<html><b>Autorzy:</b>Aleksander Józwik, Anastasiya Kryvetskaya</html>");
+        centerThisText(authors);
+        add(authors);
+
+        JLabel hyperlink = new JLabel("<html><u><b>Repozytorium projektu na GitHub</b></u></html>");
+        hyperlink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        hyperlink.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+
+                    Desktop.getDesktop().browse(new URI("https://github.com/0-Cuprum-0/jimp2-wizualizacja-grafu-java"));
+
+                } catch (IOException | URISyntaxException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+        });
+        centerThisText(hyperlink);
+        add(hyperlink);
+
+
+        add(Box.createVerticalGlue());
+        JButton closeBtn = new JButton("Zamknij");
+        closeBtn.setAlignmentX(CENTER_ALIGNMENT);
+        closeBtn.addActionListener(e -> this.dispose());
+        add(closeBtn);
+        add(Box.createVerticalStrut(20));
+    }
+
+    public Component createImageIcon(){
+        ImageIcon icon = new ImageIcon("src/images/graph_logo.png");
+
+        Image image = icon.getImage(); // transform it
+        Image newimg = image.getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+        icon = new ImageIcon(newimg);  // transform it back
+
+
+        JLabel picLabel = new JLabel(icon);
+        picLabel.setAlignmentX(CENTER_ALIGNMENT);
+        return picLabel;
+
+
+    }
+
+
+
+
+
+
+
 }
