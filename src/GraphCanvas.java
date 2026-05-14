382428618ebfd985ce4f76bf1b7e43cce686af55
Aleksander Jóźwik <jozwikaleksander@protonmail.com>
2026-05-14 11:04:21 +0200

    Poprawienie błędu z eksportowaniem grafu

--- src/GraphCanvas.java
+++ src/GraphCanvas.java
@@ -257,7 +257,7 @@ public class GraphCanvas extends JPanel {
     }
 
     public void exportToPNG(File file) throws IOException {
-        BufferedImage image = new BufferedImage(getWidth()*2, getHeight()*2, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage image = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
         
         Graphics2D g2d = image.createGraphics();
         
